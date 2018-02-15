df41bbf887e52d54616b95dfcd5e896bf3d88ace
Jeff Smith <jsmith3@vonage.com>
2018-02-15 17:11:57 -0500

    serialize blocks

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::str;
 use std::time::{SystemTime, UNIX_EPOCH};
+
+extern crate data_encoding;
+use data_encoding::HEXLOWER;
 
 extern crate ring;
 use ring::digest;
@@ -11,16 +15,51 @@ extern crate serde_json;
 extern crate serde_derive;
 
 fn main() {
-    println!("{}", timestamp());
+    let block = Block {
+        index: 1,
+        timestamp: timestamp(),
+        proof: 1,
+        previous_hash: Hash::new(b"hello world"),
+        transactions: vec![]
+    };
 
+    let block2 = Block {
+        index: 2,
+        timestamp: timestamp(),
+        proof: 1,
+        previous_hash: hash_block(&block),
+        transactions: vec![]
+    };
+    println!("{}", serde_json::to_string_pretty(&block).unwrap());
+    println!("{}", serde_json::to_string_pretty(&block2).unwrap());
 }
+
+struct Blockchain {
+    blocks: Vec<Block>
+}
+
+impl Blockchain {
+    fn new() -> Blockchain {
+        Blockchain {
+            blocks: vec![Block {
+                index: 1,
+                timestamp: timestamp(),
+                proof: 1,
+                previous_hash: Hash::new(b"first"),
+                transactions: vec![]
+            }]
+        }
+    }
+}
+
+fn append_block(blockchain: &mut Blockchain) {}
 
 #[derive(Serialize)]
 struct Block {
     index: u64,
     timestamp: f64,
     proof: u64,
-    hash: Hash,
+    previous_hash: Hash,
     transactions: Vec<Transaction>
 }
 
@@ -35,12 +74,24 @@ struct Hash {
     digest: digest::Digest
 }
 
+impl Hash {
+    fn new(bytes: &[u8]) -> Hash {
+        Hash { digest: digest::digest(&digest::SHA256, bytes) }
+    }
+}
+
 impl Serialize for Hash {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
         where S: Serializer
     {
-        serializer.serialize_bytes(self.digest.as_ref())
+        let s = HEXLOWER.encode(self.digest.as_ref());
+        serializer.serialize_str(&s)
     }
+}
+
+fn hash_block(block: &Block) -> Hash {
+    let s = serde_json::to_string(block).unwrap();
+    Hash::new(s.as_ref())
 }
 
 fn timestamp() -> f64 {
