8577f3e526b5fe335779dae168b0e344994042a1
Jeff Smith <jsmith3@vonage.com>
2018-02-23 17:00:52 -0500

    get chain

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,28 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
-extern crate serde_json;
+extern crate rocket_contrib;
 extern crate blockchain;
+#[macro_use]
+extern crate serde_json;
 
+use std::sync::Mutex;
 use blockchain::{Blockchain, Transaction, proof_of_work};
+use rocket::State;
+use rocket_contrib::{Json, Value};
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/chain")]
+fn chain(blockchain: State<Mutex<Blockchain>>) -> Json<Value> {
+    let blockchain = blockchain.lock().unwrap();
+    Json(json!({
+        "chain": blockchain.blocks,
+        "length": blockchain.len()
+    }))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![chain])
+        .manage(Mutex::new(Blockchain::new()))
+        .launch();
 }
